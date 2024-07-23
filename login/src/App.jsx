1998b48a755907df234bdfeee25652934b531ea4
afeefnk <connectafeefnk@gmail.com>
2024-07-23 22:52:41 +0530

    Login

---
+++ login/src/App.jsx
@@ -0,0 +1,33 @@
+import emailicon from '../src/Assets/email.png'
+import passwordicon from '../src/Assets/password.png'
+import './App.css';
+import Navbar from './Navbar.jsx'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <div className='main'>
+    <div className='header'>
+      <div className='text'>Login</div>
+      <div className='underline'></div>
+    </div>
+    <div className='inputs'>
+      <div className='input'>
+        <img src={emailicon} alt="" />
+        <input type="email" placeholder='Email Id' required />
+      </div>
+      <div className='input'>
+        <img src={passwordicon} alt="" />
+        <input type="password" placeholder='Password' required />
+      </div>
+    </div>
+     <div className="button">
+      <div className="submit">Login</div>
+     </div>
+   </div>
+   </>
+  );
+}
+
+export default App;
---
+++ login/src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav class="navbar">
+    <div class="menu">
+        <a href="a" class="btn">Home</a>
+        <a href="a" class="btn">About</a>
+        <a href="a" class="btn">Services</a>
+        <a href="a" class="btn">Portfolio</a>
+        <a href="a" class="btn">Contact</a>
+        <a href="a" class="btn">Signup</a>
+    </div>
+</nav>
+  )
+}
+
+export default Navbar
