0eb2be223783d73e92ec8ede7270e9dab38f6a3d
afeefnk <connectafeefnk@gmail.com>
2024-07-24 08:01:18 +0530

    Cards-app

---
+++ cards-app/src/App.jsx
@@ -0,0 +1,57 @@
+import './App.css';
+import profile from './profile.jpeg';
+import Card from './Card';
+
+function App(props) {
+  let details = [{
+    name: "Adam Johnson",
+    role: "Developer",
+    image: profile,
+    class: 'grey'
+  },
+  {
+    name: "Linda Larson",
+    role: "Manager",
+    image: profile,
+    class: 'white'
+  },
+  {
+    name: "Marry Hudson",
+    role: "Designer",
+    image: profile,
+    class: 'white'
+  },
+  {
+    name: "Nina Hudson",
+    role: "Designer",
+    image: profile,
+    class: 'white'
+  },
+  {
+    name: "Margo Larson",
+    role: "Manager",
+    image: profile,
+    class: 'white'
+  },
+  {
+    name: "Nicole Scavo",
+    role: "Designer",
+    image: profile,
+    class: 'grey'
+  }
+];
+
+  return (
+    <div className='card'>
+    {details.map((item, index) => {
+      return (
+        <>
+          <Card key = {index} name = {item.name} role = {item.role} image = {item.image} class = {item.class}/>
+        </>
+      )
+    })}
+  </div>
+  );
+}
+
+export default App;
