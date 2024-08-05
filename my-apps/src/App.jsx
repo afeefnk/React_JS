bbbaf2ec0911bfb73843d30844f1994f1a190399
afeefnk <connectafeefnk@gmail.com>
2024-08-05 13:40:25 +0530

    navbar

---
+++ my-apps/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <nav className="navbar">
+      <div className="head1">
+        <h1>Softroniics</h1>
+      </div>
+      <div>
+        <a href="$" className="btn">Home</a>
+        <a href="$" className="btn">About</a>
+        <a href="$" className="btn">Portfolio</a>
+        <a href="$" className="btn">Services</a>
+        <a href="$" className="btn">Contact</a>
+      </div>
+    </nav>
+    </>
+  );
+}
+
+export default App;
