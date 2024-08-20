cf5af009b0e6d84c4cafbe0c937d0f878896e2d6
afeefnk <connectafeefnk@gmail.com>
2024-08-20 13:58:37 +0530

    Style the button

--- my-apps/src/Components/useContext/Layout.js
+++ my-apps/src/Components/useContext/Layout.js
@@ -6,7 +6,18 @@ const Layout = () => {
   return (
     <div style={{backgroundColor:theme === 'light' ? '#fff' : '#333', color: theme === 'light' ? '#000' : '#fff', textAlign: 'center'}}>
         <h2>Layout</h2>
-        <button onClick={toggleTheme}>Click</button>
+        <button onClick={toggleTheme} style={{
+                    padding: '10px 20px',
+                    backgroundColor: theme === 'light' ? '#333' : '#fff',
+                    color: theme === 'light' ? '#fff' : '#333',
+                    border: 'none',
+                    borderRadius: '5px',
+                    cursor: 'pointer',
+                    marginTop: '20px',
+                    fontSize: '16px',
+                    transition: 'background-color 0.3s ease, color 0.3s ease',
+                    marginBottom: '10px'
+                }}>Change Theme</button>
     </div>
   )
 }
