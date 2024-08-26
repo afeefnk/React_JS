e7489a9b8846f80e6f922d5c99a267d141927d39
afeefnk <connectafeefnk@gmail.com>
2024-08-26 11:07:30 +0530

    Redux

---
+++ redux/src/Components/Store.js/Store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import CounterSlice from "../Counter Slice/CounterSlice";
+// import { counter } from "@fortawesome/fontawesome-svg-core";
+
+const store=configureStore({
+reducer:{
+    counter:CounterSlice
+}
+})
+
+export default store
