7a7cbfca7e4c6380449a792be27a3002674581e4
afeefnk <connectafeefnk@gmail.com>
2024-08-26 10:04:54 +0530

    Created a new folder redux and created a component Counter.jsx also created a file CounterSlice.jsx

---
+++ redux/src/Components/Counter/Counter.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './counter.css'
+
+const Counter = () => {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button>Click Here</button>
+    </div>
+  )
+}
+
+export default Counter
